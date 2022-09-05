54ec8b4f9e6407f35280cce4ae730adb51ef4733
Andy Huang <andyhuang1223@gmail.com>
2022-09-05 18:03:35 +0800

    finished EfRepository

---
+++ BS.DemoShop.Infrastructure/Data/EfRepository.cs
@@ -0,0 +1,75 @@
+﻿using BS.DemoShop.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS.DemoShop.Infrastructure.Data
+{
+    public class EfRepository<T> : IRepository<T> where T : class
+    {
+        private readonly BSDemoShopContext _dbContext;
+
+        public EfRepository(BSDemoShopContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _dbContext.Set<T>().AddRange(entities);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbContext.Set<T>().AddRangeAsync(entities);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _dbContext.Set<T>().RemoveRange(entities);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbContext.Set<T>();
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+        }
+
+        public void UpdateRange(IEnumerable<T> entities)
+        {
+            _dbContext.Set<T>().UpdateRange(entities);
+        }
+    }
+}
