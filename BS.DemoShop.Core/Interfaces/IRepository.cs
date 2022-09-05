6fff842c997efac17dddc0508ee1080b560534ef
Andy Huang <andyhuang1223@gmail.com>
2022-09-05 17:50:11 +0800

    IRepository API

--- BS.DemoShop.Core/Interfaces/IRepository.cs
+++ BS.DemoShop.Core/Interfaces/IRepository.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace BS.DemoShop.Core.Interfaces
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
+        IQueryable<T> GetAll();
+        void SaveChanges();
+        Task SaveChangesAsync();
     }
 }
