7c95452fb1712440f1d60ae5fcd805cc4ce0f070
Andy Huang <andyhuang1223@gmail.com>
2022-09-05 18:07:02 +0800

    新增 ToTaiwaneseTime 擴充方法

--- BS.DemoShop.Core/Extensions/TimeExtension.cs
+++ BS.DemoShop.Core/Extensions/TimeExtension.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeZoneConverter;
 
 namespace BS.DemoShop.Core.Extensions
 {
-    public class TimeExtension
+    public static class TimeExtension
     {
+        /// <summary>
+        /// UTC 時間轉為台灣時區擴充方法
+        /// </summary>
+        /// <param name="utcDateTime">UTC +0 DateTime</param>
+        /// <returns></returns>
+        public static DateTime ToTaiwaneseTime(this DateTime utcDateTime)
+        {
+            var timeZoneInfo = TZConvert.GetTimeZoneInfo("Asia/Taipei");
+            var localTime = TimeZoneInfo.ConvertTime(utcDateTime, timeZoneInfo);
+            return localTime;
+        }
     }
 }
