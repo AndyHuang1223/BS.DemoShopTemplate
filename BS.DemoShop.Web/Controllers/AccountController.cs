1b73f9bbe2ba419a607a1f13d82d45034f47d3d8
Andy Huang <andyhuang1223@gmail.com>
2022-09-11 16:23:09 +0800

    新增Cookie驗證所需設定及新增AccountController

---
+++ BS.DemoShop.Web/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BS.DemoShop.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("/signin")]
+        public IActionResult SignIn(string returnUrl)
+        {
+            return View();
+        }
+    }
+}
--- BS.DemoShop.Web/Startup.cs
+++ BS.DemoShop.Web/Startup.cs
@@ -29,6 +29,7 @@ namespace BS.DemoShop
             services.AddDbContext<BSDemoShopContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BSDemoShopConnection")));
             services.AddCoreServices();
             services.AddWebServices();
+            services.AddCookieSettings();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +47,7 @@ namespace BS.DemoShop
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
