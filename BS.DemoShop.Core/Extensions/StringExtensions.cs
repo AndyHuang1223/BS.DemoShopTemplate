1fa2cffa90cde52c9ce34530f89dd2f69137dda3
Andy Huang <55429229+AndyHuang1223@users.noreply.github.com>
2022-09-15 16:22:15 +0800

    Feature extensions (#8)
    * Add WhereIfExtensions
    
    * Add StringExtensions
    
    * Modify TimeExtensions

---
+++ BS.DemoShop.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+namespace BS.DemoShop.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string source)
+        {
+            return string.IsNullOrEmpty(source);
+        }
+    }
+}
