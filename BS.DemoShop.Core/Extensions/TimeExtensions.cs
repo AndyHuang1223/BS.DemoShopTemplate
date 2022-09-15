9e6136b0c42c24c36ed751d0deead63d8dc2f0a5
Andy Huang <andyhuang1223@gmail.com>
2022-09-15 16:21:31 +0800

    Modify TimeExtensions

--- BS.DemoShop.Core/Extensions/TimeExtensions.cs
+++ BS.DemoShop.Core/Extensions/TimeExtensions.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace BS.DemoShop.Core.Extensions
 {
-    public static class TimeExtension
+    public static class TimeExtensions
     {
         /// <summary>
-        /// UTC 時間轉為台灣時區擴充方法
+        /// UTC 時間轉為自訂台灣時區擴充方法（如果Server環境有處理時，就不需使用）
         /// </summary>
         /// <param name="utcDateTime">UTC +0 DateTime</param>
         /// <returns>Taiwanese DateTime</returns>
@@ -19,7 +19,7 @@ namespace BS.DemoShop.Core.Extensions
             string standardName = "Taipei Time";
             TimeSpan offset = new TimeSpan(08, 00, 00);
             TimeZoneInfo taipeiTimeZone = TimeZoneInfo.CreateCustomTimeZone(standardName, offset, displayName, standardName);
-           
+
             var localTime = TimeZoneInfo.ConvertTime(utcDateTime, TimeZoneInfo.Utc, taipeiTimeZone);
             return localTime;
         }
@@ -32,6 +32,16 @@ namespace BS.DemoShop.Core.Extensions
         public static DateTime ToTaiwaneseDateTime(this DateTimeOffset timeOffset)
         {
             return timeOffset.DateTime.ToTaiwaneseDateTime();
+        }
+
+        public static long ToUnixTimeMilliseconds(this DateTime dateTime)
+        {
+            return new DateTimeOffset(dateTime.ToUniversalTime()).ToUnixTimeMilliseconds();
+        }
+
+        public static long ToUnixTimeSeconds(this DateTime dateTime)
+        {
+            return new DateTimeOffset(dateTime.ToUniversalTime()).ToUnixTimeSeconds();
         }
     }
 }
