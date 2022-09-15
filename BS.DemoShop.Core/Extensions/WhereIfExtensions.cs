ef00c68c3c581afdbe34fd719cc0cdd79a0a4832
Andy Huang <andyhuang1223@gmail.com>
2022-09-15 15:50:37 +0800

    Add WhereIfExtensions

---
+++ BS.DemoShop.Core/Extensions/WhereIfExtensions.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace BS.DemoShop.Core.Extensions
+{
+    public static class WhereIfExtensions
+    {
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, Func<T, bool> predicate, bool condition)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+        
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, Func<T, int, bool> predicate, bool condition)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, Expression<Func<T, bool>> predicate, bool condition)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+        
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, Expression<Func<T, int, bool>> predicate, bool condition)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+    }
+}
