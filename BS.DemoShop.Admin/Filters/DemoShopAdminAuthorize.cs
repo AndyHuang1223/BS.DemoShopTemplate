3677019eb89ade0db632c93e89ca852842375192
Andy Huang <andyhuang1223@gmail.com>
2022-10-12 01:53:15 +0800

    調整DemoShopAdminAuthorize並加入至BaseApiController

--- BS.DemoShop.Admin/Filters/DemoShopAdminAuthorize.cs
+++ BS.DemoShop.Admin/Filters/DemoShopAdminAuthorize.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -6,17 +7,23 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace BS.DemoShop.Admin.Filters
 {
-    public class DemoShopAdminAuthorize : IAuthorizationFilter
+    public class DemoShopAdminAuthorize : Attribute, IAuthorizationFilter
     {
         public void OnAuthorization(AuthorizationFilterContext context)
         {
             if (context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any())
+            {
                 return;
+            }
 
             if (context.HttpContext.User.Identity?.IsAuthenticated ?? false)
+            {
+                ////TODO 登出後jti失效Filter(不能再登入，要取得新的)
+                //var jti = context.HttpContext.User.Claims.First(x => x.Type == JwtRegisteredClaimNames.Jti).Value;
                 return;
+            }
 
-            context.Result = new RedirectResult("/login");
+            context.Result = new ForbidResult();
         }
     }
 }
