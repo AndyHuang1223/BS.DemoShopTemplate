d85d8e37fbebf71e7c0e681e65a4cb416dd6da4e
Andy Huang <55429229+AndyHuang1223@users.noreply.github.com>
2022-10-18 01:58:31 +0800

    修正DemoShopAdminAuthorize判斷Token方法 (#14)

--- BS.DemoShop.Admin/Filters/DemoShopAdminAuthorize.cs
+++ BS.DemoShop.Admin/Filters/DemoShopAdminAuthorize.cs
@@ -2,12 +2,14 @@ using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Net;
+using System.Net.Http.Headers;
 using BS.DemoShop.Core.Entities;
 using BS.DemoShop.Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Net.Http.Headers;
 
 namespace BS.DemoShop.Admin.Filters
 {
@@ -27,14 +29,19 @@ namespace BS.DemoShop.Admin.Filters
                 return;
             }
 
+
             if (context.HttpContext.User.Identity?.IsAuthenticated ?? false)
             {
                 //登出後Token失效Filter(不能再登入，要取得新的)
-                var authorization = context.HttpContext.Request.Headers["Authorization"];
-                var token = authorization.ToString().Split("bearer ").Last();
-                if (!_blockToken.Any(x => x.Token.ToLower() == token.ToLower()))
+                var authorization = context.HttpContext.Request.Headers[HeaderNames.Authorization];
+                if (AuthenticationHeaderValue.TryParse(authorization, out var headerValue))
                 {
-                    return;
+                    var scheme = headerValue.Scheme;
+                    var parameter = headerValue.Parameter;
+                    if (!_blockToken.Any(x => x.Token.ToLower() == parameter.ToLower()))
+                    {
+                        return;
+                    }
                 }
             }
 
