01e845a623f90a1fca1a8cebf3baf4d430c43e9a
andyhuang <andyhuang1223@gmail.com>
2023-03-20 18:27:01 +0800

    移除IProductService Create 相關方法

--- DemoShop.ApplicationCore/Interfaces/ProductService/IProductService.cs
+++ DemoShop.ApplicationCore/Interfaces/ProductService/IProductService.cs
@@ -7,8 +7,6 @@ public interface IProductService
 {
     Task<Product> GetShelveProductByIdAsync(int productId);
     Task<List<Product>> GetHotSellProductListByCategoryIdAsync(int categoryId, int count);
-    Task<Product> CreateProductAsync(CreateProductInput input);
-    Task<Specification> CreateProductSpecAsync(int productId,string specificationName, string specificationValue);
     Task ShelveProductAsync(int productId);
     Task OffShelveProductAsync(int productId);
 }
