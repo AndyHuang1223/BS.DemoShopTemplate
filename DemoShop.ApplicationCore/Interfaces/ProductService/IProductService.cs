63749041605238e55748aebcf0ce2cc0c03740ab
andyhuang <andyhuang1223@gmail.com>
2023-03-21 00:46:10 +0800

    修改IProductService取得熱銷商品

--- DemoShop.ApplicationCore/Interfaces/ProductService/IProductService.cs
+++ DemoShop.ApplicationCore/Interfaces/ProductService/IProductService.cs
@@ -6,7 +6,7 @@ namespace DemoShop.ApplicationCore.Interfaces.ProductService;
 public interface IProductService
 {
     Task<Product> GetShelveProductByIdAsync(int productId);
-    Task<List<Product>> GetHotSellProductListByCategoryIdAsync(int categoryId, int count);
+    Task<List<Product>> GetHotSellProductListAsync(int count);
     Task ShelveProductAsync(int productId);
     Task OffShelveProductAsync(int productId);
 }
