24458ff0387ef17293b22232a0cfdd04a24c13fe
andyhuang <andyhuang1223@gmail.com>
2023-03-20 10:56:14 +0800

    remove old version project

--- BS.DemoShop.Core/Interfaces/IEmailSender.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace BS.DemoShop.Core.Interfaces
-{
-    public interface IEmailSender
-    {
-        Task SendEmailAsync(string email, string subject, string message);
-    }
-}
