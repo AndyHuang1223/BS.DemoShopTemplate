912d1ce95256e7b014b09f597a0970def66fb441
andyhuang <andyhuang1223@gmail.com>
2023-03-21 01:01:45 +0800

    EFRepository實作

--- DemoShop.Infrastructure/Data/EfRepository.cs
+++ DemoShop.Infrastructure/Data/EfRepository.cs
@@ -1,118 +1,148 @@
 using System.Linq.Expressions;
 using DemoShop.ApplicationCore.Entities;
 using DemoShop.ApplicationCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace DemoShop.Infrastructure.Data;
 
 public class EfRepository<T> : IRepository<T> where T : BaseEntity
 {
+    protected readonly BSDemoShopContext DbContext;
+
+    public EfRepository(BSDemoShopContext dbContext)
+    {
+        DbContext = dbContext;
+    }
+
+
     public T Add(T entity)
     {
-        throw new NotImplementedException();
+        DbContext.Set<T>().Add(entity);
+        DbContext.SaveChanges();
+        return entity;
     }
 
     public IEnumerable<T> AddRange(IEnumerable<T> entities)
     {
-        throw new NotImplementedException();
+        DbContext.Set<T>().AddRange(entities);
+        DbContext.SaveChanges();
+        return entities;
     }
 
     public async Task<T> AddAsync(T entity)
     {
-        throw new NotImplementedException();
+        DbContext.Set<T>().Add(entity);
+        await DbContext.SaveChangesAsync();
+        return entity;
     }
 
     public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
     {
-        throw new NotImplementedException();
+        DbContext.Set<T>().AddRange(entities);
+        await DbContext.SaveChangesAsync();
+        return entities;
     }
 
     public T Update(T entity)
     {
-        throw new NotImplementedException();
+        DbContext.Set<T>().Update(entity);
+        DbContext.SaveChanges();
+        return entity;
     }
 
     public IEnumerable<T> UpdateRange(IEnumerable<T> entities)
     {
-        throw new NotImplementedException();
+        DbContext.Set<T>().UpdateRange(entities);
+        DbContext.SaveChanges();
+        return entities;
     }
 
     public async Task<T> UpdateAsync(T entity)
     {
-        throw new NotImplementedException();
+        DbContext.Set<T>().Update(entity);
+        await DbContext.SaveChangesAsync();
+        return entity;
     }
 
     public async Task<IEnumerable<T>> UpdateRangeAsync(IEnumerable<T> entities)
     {
-        throw new NotImplementedException();
+        DbContext.Set<T>().UpdateRange(entities);
+        await DbContext.SaveChangesAsync();
+        return entities;
     }
 
     public void Delete(T entity)
     {
-        throw new NotImplementedException();
+        DbContext.Set<T>().Remove(entity);
+        DbContext.SaveChanges();
     }
 
     public void DeleteRange(IEnumerable<T> entities)
     {
-        throw new NotImplementedException();
+        DbContext.Set<T>().RemoveRange(entities);
+        DbContext.SaveChanges();
     }
 
     public async Task DeleteAsync(T entity)
     {
-        throw new NotImplementedException();
+        DbContext.Set<T>().Remove(entity);
+        await DbContext.SaveChangesAsync();
     }
 
     public async Task DeleteRangeAsync(IEnumerable<T> entities)
     {
-        throw new NotImplementedException();
+        DbContext.Set<T>().RemoveRange(entities);
+        await DbContext.SaveChangesAsync();
     }
+    
 
     public T GetById<TId>(TId id)
     {
-        throw new NotImplementedException();
+        return DbContext.Set<T>().Find(new object[] { id });
     }
 
     public async Task<T> GetByIdAsync<TId>(TId id)
     {
-        throw new NotImplementedException();
+        return await DbContext.Set<T>().FindAsync(new object[] { id });
     }
 
     public T FirstOrDefault(Expression<Func<T, bool>> expression)
     {
-        throw new NotImplementedException();
+        return DbContext.Set<T>().FirstOrDefault(expression);
     }
 
     public async Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> expression)
     {
-        throw new NotImplementedException();
+        return await DbContext.Set<T>().FirstOrDefaultAsync(expression);
     }
 
     public T SingleOrDefault(Expression<Func<T, bool>> expression)
     {
-        throw new NotImplementedException();
+        return DbContext.Set<T>().SingleOrDefault(expression);
     }
 
     public async Task<T> SingleOrDefaultAsync(Expression<Func<T, bool>> expression)
     {
-        throw new NotImplementedException();
+        return await DbContext.Set<T>().SingleOrDefaultAsync(expression);
     }
 
     public bool Any(Expression<Func<T, bool>> expression)
     {
-        throw new NotImplementedException();
+        return DbContext.Set<T>().Any(expression);
     }
 
     public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
     {
-        throw new NotImplementedException();
+        return await DbContext.Set<T>().AnyAsync(expression);
     }
 
     public async Task<List<T>> ListAsync()
     {
-        throw new NotImplementedException();
+        return await DbContext.Set<T>().ToListAsync();
     }
 
     public async Task<List<T>> ListAsync(Expression<Func<T, bool>> expression)
     {
-        throw new NotImplementedException();
+        return await DbContext.Set<T>().Where(expression).ToListAsync();
     }
 }
