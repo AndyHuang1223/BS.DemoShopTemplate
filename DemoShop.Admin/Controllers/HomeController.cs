a4e88a5f2900d59f68c1e0052e0b7dbbc04c5aa4
Weber Yang <weberyanglalala@gmail.com>
2024-03-26 14:56:01 +0800

    feat(admin): todo crud basic frontend

--- DemoShop.Admin/Controllers/HomeController.cs
+++ DemoShop.Admin/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace DemoShop.Admin.Controllers
             return View();
         }
 
+        public IActionResult TodoList()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
