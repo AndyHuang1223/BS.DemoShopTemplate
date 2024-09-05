96e125becd02206dce176d79e12bf2e115071648
Andy Huang <andyhuang1223@gmail.com>
2024-09-05 11:17:24 +0800

    feat: 調整`IRepository`，將非同步提取成`IAsyncRepository`

--- DemoShop.ApplicationCore/Interfaces/IRepository.cs
+++ DemoShop.ApplicationCore/Interfaces/IRepository.cs
@@ -3,27 +3,18 @@ using DemoShop.ApplicationCore.Entities;
 
 namespace DemoShop.ApplicationCore.Interfaces;
 
-public interface IRepository<T> where T : BaseEntity
+public interface IRepository<T> : IAsyncRepository<T> where T : BaseEntity
 {
     T Add(T entity);
     IEnumerable<T> AddRange(IEnumerable<T> entities);
-    Task<T> AddAsync(T entity);
-    Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities); 
     T Update(T entity);
     IEnumerable<T> UpdateRange(IEnumerable<T> entities);
-    Task<T> UpdateAsync(T entity);
-    Task<IEnumerable<T>> UpdateRangeAsync(IEnumerable<T> entities);
     void Delete(T entity);
     void DeleteRange(IEnumerable<T> entities);
-    Task DeleteAsync(T entity);
-    Task DeleteRangeAsync(IEnumerable<T> entities);
     T GetById<TId>(TId id);
     Task<T> GetByIdAsync<TId>(TId id);
     T FirstOrDefault(Expression<Func<T, bool>> expression);
-    Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> expression);
     T SingleOrDefault(Expression<Func<T, bool>> expression);
-    Task<T> SingleOrDefaultAsync(Expression<Func<T, bool>> expression);
-    bool Any(Expression<Func<T, bool>> expression);
-    Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
-    Task<List<T>> ListAsync(Expression<Func<T, bool>> expression);
+    List<T> List(Expression<Func<T, bool>> expression);
+    List<T> List();
 }
